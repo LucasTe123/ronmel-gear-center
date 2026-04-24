c32bf8cb49edc0505c1e074a15c1824a1e9e8d2e
LucasTe123 <a2024113311@estudiantes.upsa.edu,bo>
2026-04-23 20:19:24 -0400

    Remove old Expo project files and add netlify.toml to deploy PWA

--- App.js
+++
@@ -1,9 +0,0 @@
-// ============================================
-// APP.JS - Punto de entrada de la aplicación
-// ============================================
-
-import Navegacion from './src/navigation_main';
-
-export default function App() {
-  return <Navegacion />;
-}
